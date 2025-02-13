3b5846a458cb4a1590a7853ad672c588ad43e33d
Davi Vieira <145080064+davieir4@users.noreply.github.com>
2025-02-13 08:46:52 -0300

    Update ShoppingCart.java
    add name

--- p2/Atividade 2 - 2025-02-12/ShoppingCart.java
+++ p2/Atividade 2 - 2025-02-12/ShoppingCart.java
@@ -4,9 +4,6 @@ public class ShoppingCart {
     private final int customerID;
     private final ArrayList<Product> productList;
 
-    // Um atributo final de uma classe pode ter seu valor atribuído uma única vez, seja na própria declaração ou no construtor.
-   // cada usuário só pode ter um id e uma lista, então faz sentido ser final
-
     public ShoppingCart(int customerID) {
         this.customerID = customerID;
         this.productList = new ArrayList<Product>();
@@ -20,10 +17,10 @@ public class ShoppingCart {
     public String getContents(){
         StringBuilder contents = new StringBuilder();
 
-        //StringBuilder não cria uma copia pra cada append, então é melhor nesse caso
-
         for (Product product : this.productList) {
 
+            contents.append(product.getName());
+            contents.append(" - ");
             contents.append(product.getBrand());
             contents.append(": R$ ");
             contents.append(product.getPrice());
