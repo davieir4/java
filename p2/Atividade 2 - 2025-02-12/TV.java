c9cc7b1e9cfa9726191091b46624a14c70a2ea3a
Davi Vieira <145080064+davieir4@users.noreply.github.com>
2025-02-13 08:47:50 -0300

    Update TV.java
    add name

--- p2/Atividade 2 - 2025-02-12/TV.java
+++ p2/Atividade 2 - 2025-02-12/TV.java
@@ -1,11 +1,13 @@
 public class TV extends Product{
     private int inches;
 
-    public TV (String _brand, double _price, int inches ){
-        super(_brand, _price);
+    public TV (String _name, String _brand, int inches, double _price ){
+
+        super(_name, _brand, _price);
     }
 
     public int getInches(){
+
         return this.inches;
     }
 
