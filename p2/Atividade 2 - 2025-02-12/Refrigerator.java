d936d2c3143a45387b7993c3278636aa633a8c63
Davi Vieira <145080064+davieir4@users.noreply.github.com>
2025-02-13 08:45:55 -0300

    Update Refrigerator.java
    retorno de 'name'

--- p2/Atividade 2 - 2025-02-12/Refrigerator.java
+++ p2/Atividade 2 - 2025-02-12/Refrigerator.java
@@ -1,7 +1,8 @@
 public class Refrigerator extends Product{
     private int size;
-    public Refrigerator(String _brand, double _price, int size){
-        super(_brand, _price);
+    public Refrigerator(String _name, String _brand, int size, double _price){
+
+        super(_name, _brand, _price);
     }
     public int getSize(){
         return this.size;
