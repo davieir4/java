901c6217d97dea228720d19ec95d06ced80d7d7a
Davi Vieira <145080064+davieir4@users.noreply.github.com>
2025-02-13 08:44:52 -0300

    Update Product.java
    retorno do atributo name

--- p2/Atividade 2 - 2025-02-12/Product.java
+++ p2/Atividade 2 - 2025-02-12/Product.java
@@ -1,15 +1,23 @@
 public class Product {
+    protected String name; //protected -> classes herdeiras podem editar
     protected String brand;
     protected double price;
 
-    public Product(String brand, double price) {
+    public Product(String name, String brand, double price) {
+        this.name = name;
         this.brand = brand;
         this.price = price;
     }
     public String getBrand() {
+
         return brand;
     }
     public double getPrice() {
+
         return price;
+    }
+    public String getName() {
+
+        return name;
     }
 }
